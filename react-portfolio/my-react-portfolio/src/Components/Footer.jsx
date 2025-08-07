e1a5692fa2763f72cd1c9c67a0f3a3b4aa3bf49e
emilymaeleung <emily@fl1.digital>
2025-08-07 11:34:53 +0100

    Updated GitHub icon

--- react-portfolio/my-react-portfolio/src/Components/Footer.jsx
+++ react-portfolio/my-react-portfolio/src/Components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
           <p className="text-gray-400">&copy; 2025 Emily Mae. All Rights Reserved.</p>
           <div className="social-links mt-4 md:mt-0 flex space-x-4">
             <a href="https://github.com/emilymaeleung" target="_blank" rel="noopener noreferrer" className="social-link text-gray-400 hover:text-white transition-colors duration-300">
-              GitHub 🐙
+              GitHub 🐱
             </a>
             <a href="https://www.linkedin.com/in/emilymaeleung108/" target="_blank" rel="noopener noreferrer" className="social-link text-gray-400 hover:text-white transition-colors duration-300">
               LinkedIn 💼
