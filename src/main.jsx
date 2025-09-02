75a91d7f012f5b06343a7a9d00ca075086e2904c
emilymaeleung <emily@fl1.digital>
2025-09-02 22:00:27 +0100

    debug

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import Header from './Components/header.jsx';
-import Footer from './Components/Footer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
